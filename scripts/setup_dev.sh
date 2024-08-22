7e2e70c2e8577cb567d545f8f209d30630229642
Rohan Tangri <rohan.tangri@trili.tech>
2024-08-22 17:20:42 +0100

    add dev setup script

---
+++ scripts/setup_dev.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo ---------------------
+echo Installing and creating poetry virtual environment
+echo ---------------------
+curl -sSL https://install.python-poetry.org | python3 -
+poetry install
+echo ""
+
+echo ---------------------
+echo Installing pre-commit hooks
+echo ---------------------
+poetry run pre-commit install
+echo ""
+
+echo ---------------------
+echo DONE
+echo ---------------------
+
+echo ""
+echo =================================================================================
+echo ""
+
+echo A python virtual environment has been created at:
+poetry config --local virtualenvs.path
+
+echo ""
+echo To run unit tests locally:
+echo    poetry run pytest
+
+echo ""
+echo =================================================================================
+echo ""
